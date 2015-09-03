9217748e142dd3f09bccbc26989fb5d74af35fc7
Jgoodrich07 <Jeromegoodrich@gmail.com>
2015-09-02 20:18:48 -0500

    made code more expressive
    Separated classes into game, human, and computer. left BOARD constant
    out of a class

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -7,11 +7,13 @@
 # and an interface to tell us what the computer selected and asking us to select a tile
 # we need a way to keep a tally of which pieces on the board are left
 # we need a way to determine a tie or a winner.
-class Game
-  BOARD = {1 => '1', 2 => '2', 3 => '3',4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9'}
-  attr_accessor :human, :computer, :player_1, :player_2
 
-  def board_current
+BOARD = {1 => '1', 2 => '2', 3 => '3',4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9'}
+
+class Game
+  attr_accessor :human, :computer
+
+  def game_status
     @row_1 = "#{BOARD[1]} | #{BOARD[2]} | #{BOARD[3]} "
     @row_2 = "#{BOARD[4]} | #{BOARD[5]} | #{BOARD[6]} "
     @row_3 = "#{BOARD[7]} | #{BOARD[8]} | #{BOARD[9]} "
@@ -23,21 +25,7 @@ class Game
       puts @row_3
   end
 
-  def respond(response)
-    if response == 'y'
-      puts "Great let's get started"
-      p = Game.new
-    elsif response == 'n'
-      abort('goodbye')
-    else
-      # if the user doesn't put y or n in I'm having troube restarting the process - none of the loops seem to work.
-      puts "Sorry, didn't catch that, please press y for yes and n for no"
-      input = gets.chomp
-      respond(input)
-    end
-  end
-
-  def order(order)
+  def player_order(order)
     if order == 'f'
       return true
     elsif order == 's'
@@ -45,13 +33,33 @@ class Game
     else
     end
   end
+end
 
-  def human_move(number)
-    if (1..9)===(number)
-      BOARD[number] ='X'
+class Human
+
+  def says(something)
+    if something == 'y'
+      puts "Great let's get started"
+      g = Game.new
+    elsif something == 'n'
+      abort('goodbye')
+    else
+      puts "Sorry, didn't catch that, please press y for yes and n for no"
+      something = gets.chomp
+      respond(something)
     end
   end
-  def comp_move(number)
+
+  def play(move)
+    if (1..9)===(move)
+      BOARD[move] ='X'
+    end
+
+  end
+end
+
+class Computer
+  def comp_play(move)
     until (1..9) === number
       number = BOARD[rand(1..9)]
     end
@@ -60,27 +68,30 @@ class Game
   end
 end
 
-p = Game.new
+game = Game.new
+human = Human.new
+computer = Computer.new
 puts "Hello! would you like to play a game of TicTacToe? type y for yes and n for no"
-p.respond(gets.chomp)
-p.board_current
+human.says(gets.chomp)
+game.game_status
 puts "Great! would you like to play first or second? type f for first and s for second."
-input = p.order(gets.chomp)
+input = game.player_order(gets.chomp)
+
 count = 0
 while count <= 9
 
   if input == true
-  puts "Player_1 its your move. select a number (1-9)"
-  p.human_move(gets.to_i)
-  p.board_current
-  p.comp_move(rand(1..9))
-  p.board_current
+    puts "Player_1 its your move. select a number (1-9)"
+    human.play(gets.to_i)
+    game.game_status
+    computer.comp_play(rand(1..9))
+    game.game_status
   elsif input == false
-  p.comp_move(rand(1..9))
-  p.board_current
-  puts "Player_2 its your move. select a number (1-9)"
-  p.human_move(gets.to_i)
-  p.board_current
+    computer.comp_play(rand(1..9))
+    game.game_status
+    puts "Player_2 its your move. select a number (1-9)"
+    human.play(gets.to_i)
+    game.game_status
   else
     raise "error"
   end
