2b9c78747408beb3dcdae7116e99131468ac95f5
Jgoodrich07 <Jeromegoodrich@gmail.com>
2015-09-04 14:24:16 -0500

    further refactoring

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,112 +1,12 @@
 BOARD = [0,1,2,3,4,5,6,7,8,9]
-HUMAN_MOVES = []
-COMPUTER_MOVES = []
-
-module Boardlike
-
-  BOARD = [0,1,2,3,4,5,6,7,8,9]
-
-  def board_current
-    @row_1 = "#{BOARD[1]} | #{BOARD[2]} | #{BOARD[3]} "
-    @row_2 = "#{BOARD[4]} | #{BOARD[5]} | #{BOARD[6]} "
-    @row_3 = "#{BOARD[7]} | #{BOARD[8]} | #{BOARD[9]} "
-    @line = '---------'
-      puts @row_1
-      puts @line
-      puts @row_2
-      puts @line
-      puts @row_3
-  end
-end
 
 module Moveable
-  extend Boardlike
+  HUMAN_MOVES = []
+  COMPUTER_MOVES = []
+  WINNING_MOVES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 
-
-
-end
-
-module Unbeatable
-  $winning_combos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-  attr_accessor :move
-
-  def first
-    @move = [1,2,3,4,5,6,7,8,9]
-  end
-
-  def second
-    a = []
-    b = []
-    c = []
-    if HUMAN_MOVES[-1] == 5
-      winning_combos = [[1,2,3],[7,8,9],[1,4,7],[3,6,9],[1,5,9],[3,5,7]]
-    end
-    winning_combos.each do |x|
-      if x.include?(HUMAN_MOVES[-1])
-          a.push x
-          return a
-      elsif x.include?(COMPUTER_MOVES[-1])
-          b.push x
-      else
-        a.flatten.each do |n|
-          if b.flatten.include?(n)
-                c.push n
-                c -= HUMAN_MOVES
-                c -= COMPUTER_MOVES
-              end
-              return c
-            end
-          end
-        end
-      end
-
-  def dominate
-  a = []
-    $winning_combos.each do |x|
-      n = 0
-      while n <= HUMAN_MOVES.length
-        a.push x if x.include?(HUMAN_MOVES.first) && x.include?(HUMAN_MOVES.last)
-      n += 1
-      end
-    end
-    a -= HUMAN_MOVES
-    return a
-  end
-end
-
-class Game
-  include Boardlike
-
-  def player_order(order)
-    if order == 'f'
-      return true
-    elsif order == 's'
-      return false
-    else
-      puts 'please select f to play first or s to play second'
-      order = gets.chomp
-      player_order(order)
-    end
-  end
-end
-
-class Human
-  include Moveable
-
-
-  def says(something)
-    if something == 'y'
-      puts "Great let's get started"
-    elsif something == 'n'
-      abort('goodbye')
-    else
-      puts "Sorry, didn't catch that, please press y for yes and n for no"
-      something = gets.chomp
-      says(something)
-    end
-  end
-
-  def moves(move)
+  def human_move(move)
+    puts "It's your turn please pick a space on the board"
     if BOARD[move] == 'X'
       puts "Please pick a valid move"
       move = gets.to_i
@@ -121,12 +21,104 @@ class Human
     else
       raise 'error'
     end
+  end
 
+  def comp_move(move)
+      move = move.flatten
+    until (1..9) === (i = BOARD[move.sample.to_i])
+      move = move
+    end
+      COMPUTER_MOVES.push BOARD[i]
+      puts "your opponent moved to #{i}"
+      BOARD[i] = 'O'
+  end
+
+  def first_move(type_of_player)
+    if type_of_player == 'human'
+      puts 'Please choose a space on the board using the number 1-9.'
+      comp_move(BOARD)
+    elsif type_of_player == 'computer'
+      comp_move(BOARD)
+      puts "It's your turn."
+      puts 'Please choose a space on the board using the number 1-9.'
+      human_move(gets.to_i)
+    end
+  end
+
+  def second_move(type_of_player)
+    if type_of_player == 'human'
+      human_move(gets.to_i)
+    elsif type_of_player == 'computer'
+      a = []
+      if HUMAN_MOVES[-1] == 5
+        winning_moves = [[1,2,3],[7,8,9],[1,4,7],[3,6,9],[1,5,9],[3,5,7]]
+      end
+      winning_moves.each do |x|
+        a.push x if x.include?(HUMAN_MOVES[-1]) && x.include?(COMPUTER_MOVES[-1])
+      end
+      a -= HUMAN_MOVES
+      a -= COMPUTER_MOVES
+      comp_move(a)
+    end
+  end
+
+  def third_move_and_on(type_of_player)
+    if type_of_player == 'human'
+      human_move(gets.to_i)
+    elsif type_of_player == 'computer'
+      a = []
+      WINNING_MOVES.each do |x|
+        n = 0
+        while n <= HUMAN_MOVES.length
+          a.push x if x.include?(HUMAN_MOVES.first) && x.include?(HUMAN_MOVES.last)
+        n += 1
+        end
+      end
+      a -= HUMAN_MOVES
+      comp_move(a)
+    end
+  end
+end
+
+class Player
+  include Moveable
+
+  def order(order)
+    if order == 'f'
+      first_player = 'human'
+      second_player = 'computer'
+      return first_player, second_player
+    elsif order == 's'
+      return first_player = 'computer'
+      return second_player = 'human'
+    else
+      puts 'please select f to play first or s to play second'
+      order = gets.chomp
+      order(order)
+    end
+  end
+end
+
+class Game
+
+  def initialize
+    puts "Hello! would you like to play a game of TicTacToe? type y for yes and n for no"
+  end
+
+  def board_current
+    @row_1 = "#{BOARD[1]} | #{BOARD[2]} | #{BOARD[3]} "
+    @row_2 = "#{BOARD[4]} | #{BOARD[5]} | #{BOARD[6]} "
+    @row_3 = "#{BOARD[7]} | #{BOARD[8]} | #{BOARD[9]} "
+    @line = '---------'
+      puts @row_1
+      puts @line
+      puts @row_2
+      puts @line
+      puts @row_3
   end
 
   def win?
     board_s = BOARD.map {|a| a.to_s}
-
     first_digit_ary = [1,3,4,7,1,2,3,1]
     second_digit_ary = [2,5,5,8,4,5,6,5]
     third_digit_ary = [3,7,6,9,7,8,9,9]
@@ -149,19 +141,27 @@ class Human
   end
 end
 
-class Computer
-  include Unbeatable
+class Human
   include Moveable
 
-  def comp_move(move)
-      move = move.flatten
-    until (1..9) === (i = BOARD[move.sample.to_i])
-      move = move
+  def says(something)
+    if something == 'y'
+      puts "Great let's get started"
+      puts "Would you like to play first or second? type f for first and s for second."
+      answer = (gets.chomp)
+      return answer
+    elsif something == 'n'
+      abort('goodbye')
+    else
+      puts "Sorry, didn't catch that, please press y for yes and n for no"
+      something = gets.chomp
+      says(something)
     end
-      COMPUTER_MOVES.push BOARD[i]
-      puts "your opponent moved to #{i}"
-      BOARD[i] = 'O'
   end
+end
+class Computer
+
+  include Moveable
 
   def win?
     board_s = BOARD.map {|a| a.to_s}
@@ -190,13 +190,30 @@ class Computer
 end
 
 game = Game.new
+player = Player.new
 human = Human.new
 computer = Computer.new
-puts "Hello! would you like to play a game of TicTacToe? type y for yes and n for no"
-human.says(gets.chomp)
-game.game_status
-puts "Great! would you like to play first or second? type f for first and s for second."
-input = game.player_order(gets.chomp)
+x = human.says(gets.chomp) # = 'f'
+array_of_order = player.order(x)
+game.board_current
+array_of_order.each do |type_of_player|
+  player.first_move(type_of_player)
+  game.board_current
+end
+
+
+first_move[array_of_order[0],array_of_order[1]]
+  if type_of_player == 'human'
+    player.first_move('human')
+    game.win?
+    putslayer.first_move('computer')
+    game.win?
+  end
+game.board_current
+player.second_move(type_of_player)
+game.board_current
+player.third_and_on(type_of_player)
+game.board_current
 
 count = 0
 while count <= 9
