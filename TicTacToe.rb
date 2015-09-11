c72a37a3d2c09ea3b9201a896e0e7e2a16ec7e92
Jgoodrich07 <Jeromegoodrich@gmail.com>
2015-09-11 08:01:06 -0500

    started debugging
    @console = nil whenever I go back to the Game class.

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,29 +1,67 @@
-class Board
+class Game
+  def initialize(board, player, console)
+    @board = board
+    @player = player
+    @console = console
+  end
 
-  def select_space(player_type)
-    board = [0,1,2,3,4,5,6,7,8,9]
-    if player_type == human
-       selection = board[gets.to_i]
-       mark_space(selection, human)
-    elsif player_type == computer
-      return player.computer_move
+  def gameplay(player_type)
+    if player_type == "human"
+      @console.human_first_game_play
+    elsif player_type == "computer"
+      @console.computer_first_game_play
+    else
+      @console.error
     end
   end
 
-  def mark_space(selection, player_type)
-      if player_type == human
-        board[selection] = 'X'
-        console.computer_move
-      elsif player_type == computer
-        board[selection] = 'O'
-        console.game_play
+  def start
+   @console.greeting
+  end
+
+  def win?(current_board)
+
+    board_s = current_board.map {|a| a.to_s}
+    first_digit_ary = [0,2,3,6,0,1,1,0]
+    second_digit_ary = [1,4,4,7,3,4,5,4]
+    third_digit_ary = [2,6,5,8,6,7,8,8]
+
+    n = 0
+    while n <= 7
+      if board_s[(first_digit_ary[n])] + board_s[second_digit_ary[n]] + board_s[third_digit_ary[n]] == "XXX"
+        @console.outcome("winner")
+      elsif board_s[(first_digit_ary[n])] + board_s[second_digit_ary[n]] + board_s[third_digit_ary[n]] == "OOO"
+        @console.outcome("loser")
+      end
+      n += 1
+    end
+  end
+end
+
+class Board < Game
+
+  def initialize
+    @board = [0,1,2,3,4,5,6,7,8,9]
+  end
+
+  def mark_space(move, player_type)
+      if player_type == "human"
+        @board[move] = 'X'
+        current_board = @board
+        win?(current_board)
+        gameplay("computer")
+      elsif player_type == "computer"
+        @board[move] = 'O'
+        current_board = @board
+        win?(current_board)
+        gameplay("human")
       end
   end
 
   def status
-    row_1 = "#{BOARD[1]} | #{BOARD[2]} | #{BOARD[3]} "
-    row_2 = "#{BOARD[4]} | #{BOARD[5]} | #{BOARD[6]} "
-    row_3 = "#{BOARD[7]} | #{BOARD[8]} | #{BOARD[9]} "
+    row_1 = "#{@board[1]} | #{@board[2]} | #{@board[3]} "
+    row_2 = "#{@board[4]} | #{@board[5]} | #{@board[6]} "
+    row_3 = "#{@board[7]} | #{@board[8]} | #{@board[9]} "
     line = '---------'
       puts row_1
       puts line
@@ -31,53 +69,68 @@ class Board
       puts line
       puts row_3
   end
-
-
 end
 
-class Game
 
-  def initialize(board, player, computer, console)
-    @board = board
-    @player = player
-    @computer = computer
-    @console = console
-  end
-
-  def start
-    console.greeting
-  end
-
-  def win?
-
-  end
-
-  def order(order)
-    if order == first
-      console.game_play
-    elsif oder == second
-      console.computer_move
-    end
-  end
-
-end
 
 class Player
 
-  def move(type_of_player)
-
+  def initialize
+    @winning_combinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7,],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+    @human_moves = []
+    @computer_moves = []
   end
 
+  def human_move
+    move = gets.to_i
+    @human_moves.push move
+    return move
+  end
+
+  def computer_move
+    a = []
+    if @human_moves.last == 5
+      @winning_combinations = [[1,2,3],[7,8,9],[1,4,7,],[3,6,9],[1,5,9],[3,5,7]]
+      @winning_combinations.each do |i|
+        if i.include?(@human_moves[-1]) && i.include?(@computer_moves[-1])
+          a.push i
+          a -= @human_moves
+          a -= @computer_moves
+          move = a.sample
+          return move
+        end
+      end
+    elsif @computer_moves.empty?
+      available_moves= (1..9).to_a - @human_moves.last.to_a
+      move = available_moves.sample
+      return move
+    else
+      @winning_combinations.each do |i|
+        if i.include?(@human_moves[-1]) && i.include?(@human_moves[-2])
+          a.push i
+          a -= @human_moves
+          a -= @computer_moves
+          move = a.sample
+          return move
+        end
+      end
+    end
+  end
 end
 
 class ConsoleUI
 
+  def initialize(board, player)
+    @board = board
+    @player = player
+  end
+
   def greeting
     puts "Hello! would you like to play a game of TicTacToe? type y for yes and n for no"
     answer = gets.chomp
-      if answer == y
+      if answer == "y"
         choose_player
-      elsif answer == n
+      elsif answer == "n"
         abort("maybe next time...")
       else
         error
@@ -88,24 +141,58 @@ class ConsoleUI
     puts "Great! let's get started."
     puts "Would you like to play first or second? type f for first and s for second."
     answer = gets.chomp
-      if answer == f
-        new_game.order(first)
-      elsif answer == s
-        new_game.order(second)
+      if answer == "f"
+        human_first_game_play
+      elsif answer == "s"
+        computer_first_game_play
       else
         error
       end
   end
 
-  def game_play
+  def human_first_game_play
+      @board.status
       puts "Please choose a space on the board using the available numbers 1-9."
-      board.select_space(human)
-      board.mark_space
+      move = @player.human_move
+      @board.mark_space(move, "human")
   end
 
-  def computer_move
-      player.move(computer)
-      puts "your opponent moved to #{}"
+  def computer_first_game_play
+      @board.status
+      move = @player.computer_move
+      puts "your opponent moved to #{move}"
+      @board.mark_space
   end
 
+  def error
+    puts "bzzzzt error! ERROR"
+  end
+
+  def outcome(outcome)
+    if outcome == "winner"
+      puts "Congratulations you've won!!!"
+      puts "Would you like to play again? y/n"
+    elsif outcome == "loser"
+      puts "The computer has won..."
+      puts "play again? y/n"
+    elsif outcome == "tie"
+      puts "The game is a tie..."
+      puts "play again? y/n"
+    end
+    answer = gets.chomp
+      if answer == "y"
+        @game.start
+      elsif answer == "n"
+        abort("Goodbye")
+      else
+        error
+      end
+  end
 end
+
+player = Player.new
+board = Board.new
+console = ConsoleUI.new(board, player)
+game = Game.new(board, player, console)
+
+game.start
